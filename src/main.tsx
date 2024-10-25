24f35b2f613267ebd18813e91e1ad6068828cd99
Ramsey <rbissex@oleaedge.com>
2024-10-24 22:16:39 -0500

    remove bad imports

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { StrictMode } from 'react'
 // @deno-types="@types/react-dom/client"
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
